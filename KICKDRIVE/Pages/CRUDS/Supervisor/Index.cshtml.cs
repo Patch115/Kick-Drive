43b8b3070a2f63e341120596bd60c34699682d1d
Patch115 <perzdzib.115@gmail.com>
2020-07-30 16:35:29 -0500

    version 2.2
    version 2.2 del proyecto

---
+++ KICKDRIVE/Pages/CRUDS/Supervisor/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KDControl;
+using KDSevice;
+
+namespace KICKDRIVE.Pages.CRUDS.Supervisor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KDSevice.AppDbContex _context;
+
+        public IndexModel(KDSevice.AppDbContex context)
+        {
+            _context = context;
+        }
+
+        public IList<SupervisorS> SupervisorS { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            SupervisorS = await _context.SupervisorS.ToListAsync();
+        }
+    }
+}
