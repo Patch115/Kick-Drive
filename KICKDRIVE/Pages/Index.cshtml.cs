e865cfcd074ccb7d8dc9dc26b51341d362bc8b29
Patch115 <perzdzib.115@gmail.com>
2020-07-30 16:37:19 -0500

    Version 2.3
    Version 2.3 del proyecto

--- KICKDRIVE/Pages/Index.cshtml.cs
+++ KICKDRIVE/Pages/Index.cshtml.cs
@@ -4,33 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using KDControl;
-using KDSevice;
 
 namespace KICKDRIVE.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly IRepository<Login> repository;
-        public IndexModel(IRepository<Login> repository)
+        public void OnGet()
         {
-            this.repository = repository;
-        }
 
-        public IEnumerable<Login> Login { get; private set; }
-
-        public IActionResult OnGet()
-        {
-            try
-            {
-                Login = repository.GetAll();
-                return Page();
-            }
-            catch (Exception)
-            {
-                return RedirectToPage($"/NotFount");
-            }
         }
     }
 }
